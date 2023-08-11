4b1eedcd42e27b6dec749218ad62780e1aa7ab2f
hastrotech <hastrotech1@gmail.com>
2023-08-11 06:24:58 +0100

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ * Return: gives 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
