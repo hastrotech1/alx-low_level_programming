aaa11b2b317875ee325a0fc21ec7bb5182b00c71
hastrotech <hastrotech1@gmail.com>
2023-08-11 07:16:04 +0100

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ *main - prints/outputs a string
+ *Return: gives 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
