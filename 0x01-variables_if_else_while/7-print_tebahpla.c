c916547688848b725d7d1ef5e9432a700ccc85c8
hastrotech <hastrotech1@gmail.com>
2023-08-14 17:50:30 +0100

    Smile

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet
+ *
+ * Return: gives 0 as output
+ */
+
+int main(void)
+{
+	char k;
+
+	for (k = 'z'; k <= 'a'; k--)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
