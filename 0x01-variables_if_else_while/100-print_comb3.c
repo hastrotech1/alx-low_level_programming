e7102443ffdfb3cdee84f6d94f4e8bf7145b2c46
hastrotech <hastrotech1@gmail.com>
2023-08-15 00:34:40 +0100

    Inventing is a combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints possible combinations
+ *
+ * Return: gives 0 as output
+ */
+
+int main(void)
+{
+	int t;
+	int o;
+
+	for (o = '0'; o <= '9'; o++)
+	{
+		for (t = '0'; t <= '9'; t++)
+		{
+			if (!(o == '9') || (t > '8'))
+			{
+				putchar(t);
+				putchar(o);
+				if (!(o == '9' && t == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
