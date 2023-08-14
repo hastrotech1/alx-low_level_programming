fcdca9653622069867367d95b7f12cbd947f7a46
hastrotech <hastrotech1@gmail.com>
2023-08-14 15:55:47 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints Numbers
+ *
+ * Return: gives 0 as output
+ */
+
+int main(void)
+{
+	int r;
+
+	for (r = 0; r <= 10; r++)
+		printf("%d", r);
+	printf("\n");
+	return (0);
+}
