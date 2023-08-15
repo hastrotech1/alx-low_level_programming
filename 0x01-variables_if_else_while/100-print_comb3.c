3d698c259a17dd9374c5aa4cd3bb015354239aa9
hastrotech <hastrotech1@gmail.com>
2023-08-15 01:04:31 +0100

    Inventing is a combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,16 @@
 
 int main(void)
 {
-	int t = '0';
-	int o = '0';
+	int o;
+	int t;
 
-	for (o = '0'; o <= '9'; o++)
+	for (o = 0; o < 10; o++)
 	{
 		for (t = (o + 1); t < 10; t++)
 		{
-			putchar(t);
-			putchar(o);
-			if (!(o == '9' && t == '8'))
+			putchar(o + '0');
+			putchar(t + '0');
+			if (o != 8 || t != 9)
 			{
 				putchar(',');
 				putchar(' ');
