a45cc0e2fff738d16f0404b3c136a233cc86a62c
hastrotech <hastrotech1@gmail.com>
2023-08-17 03:23:17 +0100

    Battery street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - main function
+ *
+ * @n: Brgining of a number
+ *
+ * Return: gives 0 as output
+*/
+
+
+void print_to_98(int n)
+{
+	int g;
+
+	if (n < 98)
+	{
+		for (g = n; g <= 98; g++)
+		{
+			printf("%d", g);
+			if (n <= 97)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (g = n; g >= 98; g--)
+		{
+			printf("%d", g);
+			if (g >= 99)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+}
