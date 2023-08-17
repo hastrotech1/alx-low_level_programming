1108e4cd6b394674f4151f2a927d97996e01ecea
hastrotech <hastrotech1@gmail.com>
2023-08-17 03:05:58 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - adds two number
+ *
+ * @a: checks for parameter
+ *
+ * @b: checks for parameter 2
+ *
+ * Return: gives 0 as output
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
