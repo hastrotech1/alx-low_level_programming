7f02fe45520fbd8ae7b937c70106785c6d1ade5d
hastrotech <hastrotech1@gmail.com>
2023-08-17 01:40:26 +0100

    3 colors

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ *
+ * @j: checks for parameters
+ *
+ * Return: gives 0 as output
+ */
+
+int print_last_digit(int j)
+{
+	int k;
+
+	k = j;
+	if (j < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (0);
+}
