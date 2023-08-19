f544ea954fe8f61c94b96204a5a84727764a65a9
hastrotech <hastrotech1@gmail.com>
2023-08-19 05:55:24 +0100

    print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers - prints numbers form 0-9 followed by a newline
- * 
  * Return: gives 0 as output
  */
 
@@ -10,7 +9,7 @@ void print_numbers(void)
 {
 	int j;
 
-	for (j = 0; j > 10; j++)
+	for (j = 0; j < 10; j++)
 		_putchar(j + '0');
 	_putchar('\n');
 }
