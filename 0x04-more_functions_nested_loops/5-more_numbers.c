23e50b53dc47c6867b3e52bc570a1d48e6ee2c46
hastrotech <hastrotech1@gmail.com>
2023-08-19 06:05:12 +0100

    print_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers
+ *
+ * Return: gives 0 as output
+ */
+
+void more_numbers(void)
+{
+	int h;
+	int y;
+
+	for (h = 0; h < 10; h++)
+	{
+		for (h = 0; h <= 14; y++)
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
