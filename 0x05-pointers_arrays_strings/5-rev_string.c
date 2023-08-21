a14ea352d47135858948cc1f26181ac2c2ba5010
hastrotech <hastrotech1@gmail.com>
2023-08-21 23:58:18 +0100

    A good engineer thinks

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - writes a function which revers
+ *
+ * @s: the given parameter
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int j;
+	int q;
+
+	for (j = 0; s[j] != '\0'; j++)
+		q++;
+	for (j = 0; j < q / 2; j++)
+	{
+		char v;
+
+		v = s[j];
+		s[j] = s[q - 1 - j];
+		s[q - 1 - j] = v;
+	}
+}
