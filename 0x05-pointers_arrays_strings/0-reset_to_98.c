ae4cd100559dedaa37e3df8cbeba9b392c6f4811
hastrotech <hastrotech1@gmail.com>
2023-08-21 19:04:46 +0100

    battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * @n: choosen parameter
+ *
+ * Return: gives 0 as output
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
