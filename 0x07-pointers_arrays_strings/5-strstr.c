04899e56c77b9fb5e776f8c773726e8047bb06e0
hastrotech <hastrotech1@gmail.com>
2023-08-29 07:59:07 +0100

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - This function finds the first occurence of the substring needle
+ *
+ * @haystack: The pointer of the function
+ *
+ * @needle: The source of the pointer
+ *
+ * Return: Gives 0 if substring is not foun
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int u;
+
+	for (u = 0; haystack[u] != '\0'; u++)
+	{
+		char *p1 = haystack;
+		char *p2 = needle;
+
+		while (*p1 == *p2 && *p2 != '\0')
+		{
+			p1++;
+			p2++;
+		}
+		if (*p2 == '\0')
+			return (haystack);
+	}
+	return (0);
+}
