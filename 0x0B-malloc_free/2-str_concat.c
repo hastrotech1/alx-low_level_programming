922a00ad87f08d839a0c0d5cc2e49b866ea0cba4
hastrotech <hastrotech1@gmail.com>
2023-09-06 00:55:46 +0100

    str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * str_concat - This function concatenate two strings
+ *
+ * @s1: The first pointer
+ *
+ * @s2: The second pointer
+ *
+ * Return: Gives NULL as output
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int antman;
+	int hulk;
+	int deadpool;
+	char *wolverine;
+
+	antman = 0;
+	hulk = 0;
+
+	for (deadpool = 0; s1[deadpool] != '\0'; deadpool++)
+		hulk++;
+	for (deadpool = 0; s2[deadpool] != '\0'; deadpool++)
+		antman++;
+
+	if (s2 == NULL)
+		s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+
+	wolverine = malloc(sizeof(char) * (hulk + antman) + 1);
+
+	if (wolverine == NULL)
+		return (NULL);
+	for (deadpool = 0; s1[deadpool] != '\0'; deadpool = deadpool + 1)
+		wolverine[deadpool] = s1[deadpool];
+	for (deadpool = 0; s2[deadpool] != '\0'; deadpool = deadpool + 1)
+		wolverine[hulk + 1] = s2[deadpool];
+	return (wolverine);
+
+}
