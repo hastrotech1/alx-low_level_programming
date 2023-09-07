9726a26b56143644f6899c364e36cb16f1f4f852
hastrotech <hastrotech1@gmail.com>
2023-09-07 18:32:59 +0100

    silence is argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - This function prints the number of arguments
+ *
+ * @argv: This is the argument vector
+ *
+ * @argc: This is the argumeent count
+ *
+ * Return: Gives 0 as ouutput
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+		return (0);
+}
