214bd0756121c08a13b8c073170e84141427f4c0
hastrotech <hastrotech1@gmail.com>
2023-09-12 16:58:10 +0100

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - This function prints the current file
+ *
+ * Return: Gives 0 as output
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
