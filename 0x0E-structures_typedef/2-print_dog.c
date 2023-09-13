043fb18f2c6fb02532ae30c1643998b1c579fc8f
hastrotech <hastrotech1@gmail.com>
2023-09-13 03:09:04 +0100

    task 0, 1, 2, 3 and 5

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,5 +15,6 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", (d -> name) ? d -> name : "(nil)");
 		printf("Age: %f\n", (d -> age) ? d -> age : 0);
 		printf("Owner: %s\n", (d -> owner) ? d -> owner : "(nil)");
+		return (NULL);
 	}
 }
