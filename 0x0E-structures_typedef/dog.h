6bfe7c37f5a7f4ca58fb3d9753a67b6335e42adb
hastrotech <hastrotech1@gmail.com>
2023-09-13 02:55:25 +0100

    task 0, 1, 2, 3 and 5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,11 +13,16 @@
  * Return: Gives 0 on return
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
