93b8aee5497592d46313497202c8be0e89f89824
hastrotech <hastrotech1@gmail.com>
2023-09-22 05:52:24 +0100

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * fast - prints a sting
+ * first - prints a sting
  *
  * Return: Gives the output
  */
 
-void fast(void)__attribute__((constructor));
+void first(void)__attribute__((constructor));
 
-void fast(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
