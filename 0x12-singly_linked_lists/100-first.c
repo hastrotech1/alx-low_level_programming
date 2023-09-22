7b2128f4cb397026dfd139f3616157ce682d4039
hastrotech <hastrotech1@gmail.com>
2023-09-22 05:16:02 +0100

    updated

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * fast - prints a sting
+ *
+ * Return: Gives the output
+ */
+
+void fast(void)__attribute__((constructor));
+
+void fast(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
