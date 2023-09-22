82b7e96dc0202f38cd627c9da45d94045556a107
hastrotech <hastrotech1@gmail.com>
2023-09-22 05:45:08 +0100

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,13 +15,10 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-		{
 			printf("[%d] %s\n", 0, "(nil)");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
+
 		h = h->next;
 		firstNode++;
 	}
