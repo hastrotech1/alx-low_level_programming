616fc88cbef14d6f8f4734d1f1564bc46ba6659a
hastrotech1 <hastrotech1@gmail.com>
2023-09-23 15:47:34 +0100

    updated

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,13 +10,12 @@
 
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *temp_n = head;
 
 	while (head == NULL)
 	{
-		temp = head->next;
-		free(head->str);
-		free(head);
-		head = temp;
+		head = head->next;
+		free(temp_n->str);
+		free(temp_n);
 	}
 }
