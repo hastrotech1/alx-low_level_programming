9f0923a3539e668acd64fa194b850ed5053ec740
hastrotech1 <hastrotech1@gmail.com>
2023-11-28 05:27:51 +0100

    updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@
  *
  * @text_content: The string to add to the end of the file.
  *
- * Return: 1 on success and -1 on failure 
+ * Return: 1 on success and -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
