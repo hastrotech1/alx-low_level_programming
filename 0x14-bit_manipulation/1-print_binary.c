c114ac3d9f0b45a031601a58daa2d7b28e89fc26
hastrotech1 <hastrotech1@gmail.com>
2023-12-16 02:14:17 +0100

    Task 2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_bit - Returns the value of a bit at a given index
+ * @n: Integer number
+ * @index: Given index number
+ *
+ * Return: Value of the bit at index
+ *	(-1) if an error occurred
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	return ((n >> index) & 1);
+}
